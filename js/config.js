7afef925a235057d03e57dc0ed932bdd70f26c2c
erkdevcr <erk.sojo@gmail.com>
2026-05-16 18:18:21 -0600

    Add files via upload

--- js/config.js
+++ js/config.js
@@ -121,7 +121,7 @@ const CONFIG = {
 
   // ── App metadata ──────────────────────────────────────────
   APP_NAME: 'Savart',
-  VERSION:  '1.7.51',
+  VERSION:  '1.7.52',
 };
 
 /* ── Audio format detection helpers ───────────────────────── */
